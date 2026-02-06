71e0ab829f19ad84978152d4eb1168a41776653c
Séverin Lemaignan <severin@guakamole.org>
2026-02-06 10:28:25 +0100

    refactoring -- splitting manager_core.py into smaller files

--- dialogue_manager/start_manager.py
+++ dialogue_manager/start_manager.py
@@ -25,21 +25,21 @@ import rclpy
 
 from rclpy.executors import MultiThreadedExecutor
 
-import dialogue_manager.manager_core as manager_core
+import dialogue_manager.manager_node as manager_node
 
 
 def main():
     rclpy.init()
 
-    skill = manager_core.DialogueManagerNode()
+    node = manager_node.DialogueManagerNode()
     skill_executor = MultiThreadedExecutor()
-    skill_executor.add_node(skill)
+    skill_executor.add_node(node)
 
     try:
         skill_executor.spin()
     except (KeyboardInterrupt, rclpy.executors.ExternalShutdownException):
         print("Goodbye!")
-        skill.destroy_node()
+        node.destroy_node()
 
 
 if __name__ == '__main__':
